d1a2b5e18cce9f32723cfc99896292342aa1ea58
Nils <40796807+uchars@users.noreply.github.com>
2024-12-07 17:11:36 +0100

    update'

--- config.def.h
+++ config.def.h
@@ -78,7 +78,7 @@ static const Key keys[] = {
 	{MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
 	{MODKEY | ShiftMask, XK_l, spawn, {.v = lockcmd}},
 	{MODKEY | ShiftMask, XK_s, spawn, {.v = screenshotcmd}},
-	{MODKEY | ShiftMask, XK_space, spawn, {.v = kbdswitchcmd}},
+	{Mod1Mask | ShiftMask, XK_space, spawn, {.v = kbdswitchcmd}},
 	{MODKEY, XK_b, togglebar, {0}},
 	{MODKEY, XK_j, focusstack, {.i = +1}},
 	{MODKEY, XK_k, focusstack, {.i = -1}},
