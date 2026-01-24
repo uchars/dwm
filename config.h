5a7c918308e1110c19ef9f2a2288cf22515db451
Nils <40796807+uchars@users.noreply.github.com>
2026-01-24 20:46:49 +0100

    add status2d(customized)

--- config.h
+++ config.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "sysscratch",  NULL,       NULL,       0,       1,           0 },
 };
 
 /* layout(s) */
